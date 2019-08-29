239cee7c8d85d88170111da00a0c1aa7be90445a
Alexander Selzer <aselzer3@gmail.com>
2019-08-29 11:03:05 +0200

    Added SQLQuery stub class, modified gitignore

---
+++ src/main/java/query/SQLQuery.java
@@ -0,0 +1,18 @@
+package query;
+
+import hypergraph.Hypergraph;
+
+public class SQLQuery {
+    private String query;
+    private DBSchema schema;
+
+    public SQLQuery(String query, DBSchema schema) {
+
+    }
+
+    public Hypergraph toHypergraph() {
+
+
+        return null;
+    }
+}
