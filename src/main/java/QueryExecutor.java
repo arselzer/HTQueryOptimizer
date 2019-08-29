9ef3115df7f28d55980bd578e0b181ce9e96cd7f
Alexander Selzer <aselzer3@gmail.com>
2019-08-29 20:28:09 +0200

    Implemented schema extraction from database

--- src/main/java/QueryExecutor.java
+++ src/main/java/QueryExecutor.java
@@ -1,16 +1,25 @@
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import at.ac.tuwien.dbai.hgtools.sql2hg.Schema;
+import org.postgresql.copy.CopyManager;
+import query.DBSchema;
+import query.Table;
+
+import java.util.List;
+import java.sql.*;
+import java.util.LinkedList;
 
 public class QueryExecutor {
     private Connection connection;
+    private DBSchema schema;
 
-    public QueryExecutor(Connection connection) {
+    public QueryExecutor(Connection connection) throws SQLException {
         this.connection = connection;
+
+        extractSchema();
+        System.out.println(schema);
     }
 
     public PreparedStatement execute(PreparedStatement ps) throws SQLException {
-        String query = ps.toString(); // Should work with postgres and mysql drivers
+        String query = ps.toString(); // Should work with postgres (and mysql) drivers
 
         ps.execute();
 
@@ -18,11 +27,40 @@ public class QueryExecutor {
     }
 
     public PreparedStatement execute(String query) throws SQLException {
-
         PreparedStatement ps = connection.prepareStatement(query);
 
         ps.execute();
 
         return ps;
+    }
+
+    private void extractSchema() throws SQLException {
+        schema = new DBSchema();
+
+        DatabaseMetaData metaData = connection.getMetaData();
+
+        ResultSet rs = metaData.getTables(connection.getCatalog(), "public", "%", new String[]{"TABLE"});
+        //ResultSet rs = metaData.getTables(null, null, "%", null);
+
+        List<Table> schemaTables = new LinkedList<>();
+        while (rs.next()) {
+            String tableName = rs.getString(3);
+            if (!tableName.startsWith("sql_")) {
+                // Ignore internal tables
+
+                Table table = new Table();
+                table.setName(tableName);
+
+                ResultSet rs2 = metaData.getColumns(null, null, tableName, null);
+                LinkedList<String> tableColumns = new LinkedList<>();
+                while (rs2.next()) {
+                    tableColumns.add(rs2.getString(4));
+                }
+                table.setColumns(tableColumns);
+                schemaTables.add(table);
+            }
+        }
+
+        schema.setTables(schemaTables);
     }
 }
