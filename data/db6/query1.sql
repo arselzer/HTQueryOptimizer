e8aa016c27623395cf2cf3bf1c765432ada5c2de
Alexander Selzer <aselzer3@gmail.com>
2021-05-06 11:19:58 +0200

    add option to keep existing tables
    Former-commit-id: a13007b6e993f24030322f27f009975ef5e0298c
    Former-commit-id: b352007c9ce7b6b88fb2b6503364ffd02de62e39
    Former-commit-id: 2c26b17ff1e24d20c9732ceb2a70d69e6c3d50f5

--- data/db6/query1.sql
+++ data/db6/query1.sql
@@ -1,24 +1,30 @@
 select c1a.x, c1b.x, c3b.x, c5a.x
 from c1a, c1b, c1c, c2a, c2b, c2c, c3a, c3b, c3c, c4a, c4b, c4c, c5a, c5b, c5c, c6a, c6b, c6c
 where
-      c1a.x = c1b.y
+        c1a.x = c1b.y
     and c1b.x = c1c.y
     and c1c.x = c1a.y
+
     and c2a.x = c2b.y
     and c2b.x = c2c.y
     and c2c.x = c2a.y
+
     and c3a.x = c3b.y
     and c3b.x = c3c.y
     and c3c.x = c3a.y
+
     and c4a.x = c4b.y
     and c4b.x = c4c.y
     and c4c.x = c4a.y
+
     and c5a.x = c5b.y
     and c5b.x = c5c.y
     and c5c.x = c5a.y
+
     and c6a.x = c6b.y
     and c6b.x = c6c.y
     and c6c.x = c6a.y
+
     and c1a.z = c2b.z
     and c2a.z = c3b.z
     and c3a.z = c4b.z
